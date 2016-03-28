e9e31c876fc21e49d24005de5ef3c022e7865f18
Nicolas Limpert <nicolas.limpert@alumni.fh-aachen.de>
2016-03-28 15:46:30 +0200

    DFS: cleanup code

--- dfs.cpp
+++ dfs.cpp
@@ -1,4 +1,5 @@
 #include "dfs.h"
+//#define SHOWCYCLES
 
 DFS::DFS() {
 
@@ -26,37 +27,9 @@ void DFS::visit(Node* node) {
         if (!next_node->get_visited()) {
             visit(next_node);
         } else {
-            cout << "cycle!" << endl;
+#ifdef SHOWCYCLES
+            cout << "cycle: " << next_node->get_value() << " -> ";
+#endif
         }
     }
 }
-
-//void DFS::perform_iterative_BFS(double start_node_value) {
-//    Node* cur_node = _graph->get_node(start_node_value);
-
-//    nodes_queue.push(cur_node);
-//    cur_node->set_visited(true);
-
-//    cout << endl << "iterative BFS!" << endl;
-
-//    while (!nodes_queue.empty()) {
-//        cur_node = nodes_queue.front();
-//        nodes_queue.pop();
-
-//        cout << "cur_node: " << cur_node->get_value() << endl;
-
-////        if (cur_node == goal_node) {
-////            return;
-////        }
-//        vector<Edge*> cur_edges = cur_node->get_edges();
-//        Node* next_node;
-
-//        for (uint iE = 0; iE < cur_edges.size(); iE++) {
-//            next_node = cur_edges[iE]->get_right_node();
-//            if (!next_node->get_visited()) {
-//                next_node->set_visited(true);
-//                nodes_queue.push(next_node);
-//            }
-//        }
-//    }
-//}
